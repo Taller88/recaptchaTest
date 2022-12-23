69e2db8e3dd1ec623f96a1be6b4ef873c4b6b567
Taller88 <iod1124@naver.com>
2022-12-23 17:36:29 +0900

    add endPoint

--- app.js
+++ app.js
@@ -3,21 +3,28 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 
 const app = express();
-app.set('port', process.env.ports||9180);
+app.set('port', process.env.ports||8080);
 app.set('view engine','html');
 nunjucks.configure('views',{
     express:app,
     watch:true
 });
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
+
+app.get("/", (req, res, next)=>{
+    console.log("recaptcha Test: "+ process.env.ports);
+
+    res.send("recaptcha");
+});
 app.get("/recaptcha", (req, res, next)=>{
     console.log("recaptcha Test");
     res.render("recaptcha");
-})
+});
 
 app.listen(app.get('port'),()=>{
     console.log("[Server] open : "+ app.get('port'))
