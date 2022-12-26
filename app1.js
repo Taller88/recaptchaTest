0eefc056dedf7ab4603ddca48a8463473801c404
Taller88 <iod1124@naver.com>
2022-12-26 21:11:46 +0900

    analyze recaptcha

--- app1.js
+++ app1.js
@@ -21,11 +21,24 @@ app.get("/", (req, res, next)=>{
 
     res.send("recaptcha");
 });
+app.get("/recaptcha/api2/anchor", (req, res, next)=>{
+    console.log("/recaptcha/api2/anchor Test");
+    //'https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Lc3fgATAAAAAGs68Yfc6gFlTP0LK-KtWj1AD7C7&co=aHR0cDovLzEyNy4wLjAuMTo1NTAw&hl=ko&v=5qcenVbrhOy8zihcc2aHOWD4&size=normal&cb=hmxax0bzqhc8'
+    // http://localhost:8080/recaptcha/api2/anchor?ar=1&k=6Lc3fgATAAAAAGs68Yfc6gFlTP0LK-KtWj1AD7C7&co=aHR0cDovLzEyNy4wLjAuMTo1NTAw&hl=ko&v=5qcenVbrhOy8zihcc2aHOWD4&size=normal&cb=hmxax0bzqhc8
+    res.render("anchor");
+});
+
 app.get("/recaptcha", (req, res, next)=>{
     console.log("recaptcha Test");
     res.render("recaptcha");
 });
 
+app.get("/worker", (req, res, next)=>{
+    console.log("worker endpoint Test");
+    res.send("importScripts('http://localhost:5500/recaptcha/recaptcha__ko.js');");
+});
+
+//
 app.listen(app.get('port'),()=>{
     console.log("[Server] open : "+ app.get('port'))
 })
---
+++ recaptcha/recaptchaInit.js
@@ -0,0 +1,32 @@
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */
+(function() {
+    var w = window,
+        C = '___grecaptcha_cfg',
+        cfg = w[C] = w[C] || {},
+        N = 'grecaptcha';
+    var gr = w[N] = w[N] || {};
+    gr.ready = gr.ready || function(f) {
+        (cfg['fns'] = cfg['fns'] || []).push(f);
+    };
+    w['__recaptcha_api'] = 'https://www.google.com/recaptcha/api2/';
+    (cfg['render'] = cfg['render'] || []).push('explicit');
+    (cfg['onload'] = cfg['onload'] || []).push('onloadCallback');
+    w['__google_recaptcha_client'] = true;
+    var d = document,
+        po = d.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    // po.src = 'https://www.gstatic.com/recaptcha/releases/5qcenVbrhOy8zihcc2aHOWD4/recaptcha__ko.js';
+    po.src = 'http://localhost:5500/recaptcha/recaptcha__ko.js';
+    po.crossOrigin = 'anonymous';
+    // po.integrity = 'sha384-F/krafqJDpE6x/XFJrR7nhKFPjJFWmGaebsBaHWVzifhscYvXggYXsZ2Hje87Cwpfk';
+    //  sha384-F/PLGFRzPeAh8bjr3tMCNSuIVmi/SLOflBsjuQppCRMXACSzr4eiYbcsAvUh2ZKz
+    //krafqJDpE6x/XFJrR7nhKFPjJFWmGaebsBaHWVzifhscYvXggYXsZ2Hje87Cwpfk
+    var e = d.querySelector('script[nonce]'),
+        n = e && (e['nonce'] || e.getAttribute('nonce'));
+    if (n) {
+        po.setAttribute('nonce', n);
+    }
+    var s = d.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+})();
